4d200c9bed8ea0678341e935037fbc78c14c983b
Tatu Pesonen <tatu@narigon.dev>
2023-03-21 20:21:40 +0200

    fix: add plugins and zsh config

--- .install-config.sh
+++ .install-config.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 set -e
+DOTFILES_PATH="$HOME/.dotfiles"
+PKGS="stow zsh pkg ripgrep curl"
+
+
+clone_repo() {
+  echo "🔨 Pulling dotfiles to $DOTFILES_PATH"
+  git clone --progress https://github.com/tatupesonen/dotfiles.git $DOTFILES_PATH
+}
 
 install_pkg_debian() {
- local pkgs="stow zsh"
  echo "📦 Installing packages"
- echo "   $pkgs"
- sudo apt-get -y -qq install $pkgs 1> /dev/null
+ echo "   $PKGS"
+ sudo apt-get -y -qq install $PKGS
  echo "✅ Packages installed!"
 }
 
+install_omz() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
 mount_cfg() {
-  echo "Will mount configuration from $HOME/.dotfiles to $HOME/"
+  echo "Will mount configuration from $DOTFILES_PATH to $HOME/"
   while true; do
   read -p "Do you want to mount the dotfiles? (yes/no) " yn
   case $yn in 
@@ -21,10 +32,16 @@ mount_cfg() {
           * ) echo invalid response;;
   esac
   done
-  stow --dir=$HOME/.dotfiles --target=$HOME/ .
+  stow --dir=$DOTFILES_PATH --target=$HOME/ .
 }
 
+change_shell() {
+  chsh -s $(which zsh)
+}
 
 install_pkg_debian
+clone_repo
 mount_cfg
+change_shell
+install_omz
 echo "✅ Done!"
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # .dotfiles
 
 ## Installation
+## Quick
+Quick install:
+```
+curl -s -L https://raw.githubusercontent.com/tatupesonen/dotfiles/main/.install-config.sh | bash
+```
+## Manual
 Clone the repo to `$HOME/.dotfiles` and run `.install-config.sh`
