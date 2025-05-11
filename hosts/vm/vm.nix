37cae40c273442f6261e30ea631899c9749d6697
Tatu Pesonen <tatu@narigon.dev>
2025-05-11 23:45:22 +0300

    Update vm.nix

--- hosts/vm/vm.nix
+++ hosts/vm/vm.nix
@@ -20,9 +20,6 @@
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
