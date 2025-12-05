a1b5c30098e05f114040c4e2b22adfe13d512ac9
Tatu Pesonen <tatu@narigon.dev>
2025-12-05 10:25:14 +0200

    fix: new hw

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "NixOS Flake for @tatupesonen configs";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.05";
+      url = "github:nix-community/nixvim/nixos-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     vscode-server = {
